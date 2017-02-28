b7cce99619baf9fc9a24354013800cb1b44a5b26
peheenueenuee <pehee.nuee.nuee@gmail.com>
2017-02-28 15:23:45 +0900

    add power procedure in number.rkt

--- number.rkt
+++ number.rkt
@@ -5,6 +5,16 @@
 (define (=< a b) (or (= a b) (< a b)))
 (define (=> a b) (or (= a b) (> a b)))
 
+(define (average a b) (/ (+ a b) 2))
+(define (** a n)
+  (cond ((= n 0) 1)
+        ((even? n) (square (** a (/ n 2))))
+        (else (* a (** a (dec n))))))
+(define (*** a n)
+  (cond ((=< n 1) a)
+        ((= n 2) (** a a))
+        (else (** a (*** a (dec n))))))
+
 (define (ennumerate-interval a b)
   (if (> a b) null
       (cons a (ennumerate-interval (inc a) b))))
